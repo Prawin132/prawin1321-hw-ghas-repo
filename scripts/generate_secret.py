ee7dd7ef127b0083f1ee304ddb6da2e3feb9d426
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2025-06-13 15:03:20 +0530

    Update generate_secret.py

--- scripts/generate_secret.py
+++ scripts/generate_secret.py
@@ -4,3 +4,4 @@ import rstr
 print(f"AWS_KEYID: {rstr.xeger(r'AKIA[A-Z2-7]{16}')}")
 print(f"AWS_SECRET: {rstr.xeger(r'[0-9A-Za-z/+=]{40}')}")
 print(f"DATADOG_TOKEN: {rstr.xeger(r'[a-f0-9]{32}|[a-f0-9]{40}')}")
+ptint(f"GitHubToken:github_pat_wt6lVzza0QFx/U33PU8DrkMbnKiu+bv9jheR0h)
